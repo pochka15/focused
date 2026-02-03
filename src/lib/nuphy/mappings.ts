d52426ad85dc01643aa3783074e8c80d526ae72f
Artsiom Kashkou <schetka40@gmail.com>
2026-02-03 17:12:44 +0100

    Fix nuphys order

--- src/lib/nuphy/mappings.ts
+++ src/lib/nuphy/mappings.ts
@@ -4,10 +4,10 @@ export const nuphyPriorities = {
   root: 1,
   tablePanel: 2,
   addTodo: 3,
-  help: 4,
-  notesPanel: 5,
-  command: 6,
-  canvasBoard: 7,
+  canvasBoard: 4,
+  help: 5,
+  notesPanel: 6,
+  command: 7,
 } as const;
 
 export type KnownNuphy = keyof typeof nuphyPriorities;
